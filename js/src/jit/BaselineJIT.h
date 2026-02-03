cd6e2edbaa528a45de542b1d264d78f1c1f9a561
Justin <meimar@ualberta.ca>
2026-02-03 12:41:26 -0700

    clean up dead code

--- js/src/jit/BaselineJIT.h
+++ js/src/jit/BaselineJIT.h
@@ -533,8 +533,6 @@ class BaselineInterpreter {
 
 #if defined(ENABLE_JS_AOT_ICS) || defined(ENABLE_AOT_BASELINE)
   // Patch entries for runtime pointer relocation (AOT only).
-  // using TablePatchVec = Vector<DispatchTablePatch, 0, SystemAllocPolicy>;
-  // TablePatchVec tablePatches;
   using RuntimePatchVec = Vector<RuntimePatch, 0, SystemAllocPolicy>;
   RuntimePatchVec runtimePatches;
 #endif
