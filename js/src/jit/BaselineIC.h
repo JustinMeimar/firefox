2ceab7e3a6d634dd607f7df31260a30fba8dda0a
justinmeimar <meimar@ualberta.ca>
2026-04-12 12:35:05 -0600

    squash me: part 5

--- js/src/jit/BaselineIC.h
+++ js/src/jit/BaselineIC.h
@@ -211,6 +211,8 @@ class ICStub {
   inline void incrementEnteredCount() { enteredCount_++; }
   void resetEnteredCount() { enteredCount_ = 0; }
 
+  bool isStaticCode() const;
+
   uint8_t* stubCodeRaw() const { return stubCode_; }
 
   static constexpr size_t offsetOfStubCode() {
